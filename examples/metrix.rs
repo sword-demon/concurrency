be7486439078c2dbbd74aaeacb2fbcfc4e85cac2
wxvirus <1418667580@qq.com>
2024-07-28 13:57:34 +0800

    fix: unwrap change to ?

--- examples/metrix.rs
+++ examples/metrix.rs
@@ -11,10 +11,10 @@ fn main() -> Result<()> {
 
     // start N workers and M requests
     for idx in 0..N {
-        task_worker(idx, metrix.clone()); // Arc::clone(&metrix)
+        task_worker(idx, metrix.clone())?; // Arc::clone(&metrix)
     }
     for _ in 0..M {
-        process_requests(metrix.clone());
+        request_worker(metrix.clone())?;
     }
 
     loop {
@@ -23,21 +23,32 @@ fn main() -> Result<()> {
     }
 }
 
-fn task_worker(idx: usize, metrics: Metrics) {
-    thread::spawn(move || loop {
-        let mut rng = rand::thread_rng();
-        // do long term stuff
-        thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
-        metrics.inc(format!("call.thread.worker.{}", idx)).unwrap();
+fn task_worker(idx: usize, metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::thread_rng();
+            // do long term stuff
+            thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
+            metrics.inc(format!("call.thread.worker.{}", idx))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
     });
+    Ok(())
 }
 
-fn process_requests(metrics: Metrics) {
-    thread::spawn(move || loop {
-        let mut rng = rand::thread_rng();
-        // do long term stuff
-        thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
-        let page = rng.gen_range(1..256);
-        metrics.inc(format!("req.page.{}", page)).unwrap();
+fn request_worker(metrics: Metrics) -> Result<()> {
+    thread::spawn(move || {
+        loop {
+            let mut rng = rand::thread_rng();
+            // do long term stuff
+            thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
+            let page = rng.gen_range(1..256);
+            metrics.inc(format!("req.page.{}", page))?;
+        }
+        #[allow(unreachable_code)]
+        Ok::<_, anyhow::Error>(())
     });
+
+    Ok(())
 }
